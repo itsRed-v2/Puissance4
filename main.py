31ce58a16d54811d9d2b040d914d81a6c22f86cd
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-01-17 20:02:49 +0100

    Refactorisation de facon à pouvoir intégrer deux joueurs différents

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from ast import match_case
+from random import randint
+from time import sleep
+
 from p4.board import Board
 from p4.utils.vector import Vector
 from p4.utils.token import Token
@@ -12,24 +14,23 @@ from p4.strikeDetector import detectStrike
 
 board = Board()
 
-# ================================
-
 playing = True
 
 footer = ""
 
 tour = Token.YELLOW
 
-while playing:
+# ==== Functions ====
 
-	displayGame(board, footer, tour)
-
-	footer = ""
+def userInput():
+	global playing
+	global tour
+	global footer
 
 	reponse = input("-> ")
 
 	if reponse == "":
-		continue
+		return
 
 	elif reponse == "stop":
 		playing = False
@@ -43,18 +44,41 @@ while playing:
 		if value < 1 or value > 7:
 			footer = Color.RED + "Nombre invalide: le nombre doit être un entier compris entre 1 et 7"
 		else:
-			l = board.addToken(value - 1, tour)
-
-			if detectStrike(board, Vector(value - 1, l), tour):
-				playing = False
-
-				print(Color.RESET + "\n")
-				displayBoard(board)
-				print(f"\nJoueur {tokenToString(tour)} a gagné!")
-
-			if tour == Token.YELLOW: 
-				tour = Token.BLUE
-			else: tour = Token.YELLOW
+			return value
 
 	else:
 		footer = Color.RED + "Argument invalide"
+
+# ==== Main loop ====
+
+while playing:
+
+	displayGame(board, footer, tour)
+
+	footer = ""
+
+	match tour:
+		case Token.YELLOW:
+			answer = userInput()
+		case Token.BLUE:
+			sleep(1)
+			answer = randint(1, Board.WIDTH)
+
+	if not type(answer) is int:
+		continue
+
+	row = board.addToken(answer - 1, tour)
+
+	if row == -1 or row == None:
+		continue
+
+	if detectStrike(board, Vector(answer - 1, row), tour):
+		playing = False
+
+		print(Color.RESET + "\n")
+		displayBoard(board)
+		print(f"\nJoueur {tokenToString(tour)} a gagné!")
+
+	if tour == Token.YELLOW:
+		tour = Token.BLUE
+	else: tour = Token.YELLOW
