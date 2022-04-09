4270d51f645d46c278fd632d342d27c0179b98db
itsRed-v2 <paul.charignon.v2@gmail.com>
2022-04-09 11:06:02 +0200

    L'AI ne joue plus de colonne pleine

--- p4/players/IAplayer.py
+++ p4/players/IAplayer.py
@@ -4,6 +4,22 @@ from time import sleep
 from p4.players.player import Player
 
 class IAPlayer(Player):
-	def play(self, board):
-		sleep(.5)
-		return randint(1, board.WIDTH)
+	def play(self, board, randint = randint, doSleep = True):
+		if doSleep: sleep(.5)
+
+		# Initialisation
+		playable = []
+		for i in range(board.WIDTH):
+			playable.append(i)
+
+		# Suppression des colonnes pleines
+
+		def filterFull(colIndex):
+			firstEmpty = board.getFirstEmpty(colIndex)
+			return firstEmpty != None and firstEmpty != -1
+
+		playable = filter(filterFull, playable)
+
+		# Selection au hasard
+		playable = list(playable)
+		return playable[randint(0, len(playable) - 1)] + 1
---
+++ test/players/test_IAplayer.py
@@ -0,0 +1,53 @@
+from p4.board import Board
+
+from p4.players.IAplayer import IAPlayer
+
+from p4.utils.token import Token
+
+def test_randomPlay():
+	IA = IAPlayer(Token.BLUE, "IA")
+
+	board = Board([
+		"       ",
+		"       ",
+		"       ",
+		"       ",
+		"BY   B ",
+		"YYBBYYB"
+	])
+
+	randomRuns = 0
+
+	def mockRandom(a, b):
+		nonlocal randomRuns
+		randomRuns += 1
+		assert a == 0
+		assert b == 6
+		return 0
+
+	assert IA.play(board, mockRandom, False) == 1
+	assert randomRuns == 1
+
+def test_fullColumns():
+	IA = IAPlayer(Token.BLUE, "IA")
+
+	board = Board([
+		"BBYY BY",
+		"YYBBYYB",
+		"BBYYBBY",
+		"YYBBYYB",
+		"BBYYBBY",
+		"YYBBYYB"
+	])
+
+	randomRuns = 0
+
+	def mockRandom(a, b):
+		nonlocal randomRuns
+		randomRuns += 1
+		assert a == 0
+		assert b == 0
+		return 0
+
+	assert IA.play(board, mockRandom, False) == 5
+	assert randomRuns == 1
